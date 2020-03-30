ab1d401ae3408e9a8227ffe7e78075db1187f9b7
Ellen Hartstack <hartell88@gmail.com>
2020-03-30 12:23:05 -0500

    Standardizing all photo sizes across website.

--- assets/scripts/resize_photos.sh
+++ assets/scripts/resize_photos.sh
@@ -12,11 +12,14 @@ orig_photo_name=$1
 
 #Variables:
 #Maximum Placeholder Size
-max_p_width=680
-max_p_height=480
+max_p_width=1280
+max_p_height=720
 #Maximum Thumbnail Sizes:
-max_t_width=300
-max_t_height=300
+max_t_width=768
+max_t_height=432
+#Maxiumum 2X Thumb Size:
+max_tt_width=1024
+max_tt_height=576
 
 #Check if file name exists.
 if [ ! -f "$orig_photo_name" ]; then
@@ -29,6 +32,7 @@ fi
 #Get the New Placehold filename:
 p_photo_name="${orig_photo_name%.*}_placehold.jpg"
 t_photo_name="${orig_photo_name%.*}_thumb.jpg"
+tt_photo_name="${orig_photo_name%.*}_thumb@2x.jpg"
 
 #Print out the size of the photo given
 printf "Original Image Size: "
@@ -51,4 +55,13 @@ convert "$orig_photo_name" -resize "$max_t_width"x"$max_t_height"\> "$t_photo_na
 printf "\nImage Resized to: "
 identify -format "%wx%h" "$t_photo_name"
 printf " and saved to: $t_photo_name \n"
+printf "\n"
+
+#Create a resized 2x thumbnail image:
+printf "\nCreating 2x thumbnail image...\n"
+printf "Resizing to $max_tt_width x $max_tt_height"
+convert "$orig_photo_name" -resize "$max_tt_width"x"$max_tt_height"\> "$tt_photo_name"
+printf "\nImage Resized to: "
+identify -format "%wx%h" "$tt_photo_name"
+printf " and saved to: $tt_photo_name \n"
 printf "\n"
